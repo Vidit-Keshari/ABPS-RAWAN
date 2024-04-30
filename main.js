232227da7b6c85d35de26162809fb26126a9b3ed
Vidit Keshari <95676105+Vidit-Keshari@users.noreply.github.com>
2024-04-30 11:26:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.error("error type 'just for show':- admission form");
+console.log("Admission form is just for show and doesn't works");
+
+function admission() {
+    window.location = "form.html";
+}
+
+function info(){
+    document.getElementById("info").innerHTML = "The above written details are sent to the school";
+}
+
+function homepage() {
+    window.location = "index.html";
+}
+
+function teachde() {
+    window.location = "teacher's detail.html";
+}
+
+function about() {
+    window.alert("This webpage is created by Vidit Keshari class 8 'A' (2024-25), copyright, @All rights reserved");
+}
